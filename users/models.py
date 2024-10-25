0d9777ead9d17c11a16a93e43b120c04d26c0912
Tomilola Oluwafemi <93817917+Tomilola-ng@users.noreply.github.com>
2024-10-25 10:40:04 +0000

    Added Signals

--- users/models.py
+++ users/models.py
@@ -1,35 +1,38 @@
+""" Models """
 from django.db import models
 from django.contrib.auth.models import User
 from .choices import localGovts, states, gender
 
 
 class Profile(models.Model):
+    """ Profile Model """
     user = models.OneToOneField(
-        User, 
-        on_delete=models.CASCADE)
+        User,
+        on_delete=models.CASCADE, related_name='profile')
     picture = models.ImageField(
-        default = 'default.jpg', 
+        default='default.jpg',
         upload_to='profile_pics')
     local_government = models.CharField(
-        choices = localGovts, 
-        default='Agege', 
-        max_length=50, 
+        choices=localGovts,
+        default='Agege',
+        max_length=50,
         verbose_name='Local Government you reside')
     state = models.CharField(
-        choices=states , 
+        choices=states,
         default='Ogun',
-        max_length=50, 
+        max_length=50,
         verbose_name='State you reside?')
     gender = models.CharField(
         choices=gender,
-        max_length=6, 
+        max_length=6,
         default='Male')
     bio = models.TextField(
-        verbose_name='Write a few things about yourself')
+        verbose_name='Write a few things about yourself', blank=True)
     phone = models.PositiveBigIntegerField(
         verbose_name='Phone Number',
-        default=''
-    )
+        blank=True,
+        null=True)
+    objects = models.Manager()
 
     def __str__(self):
-        return f'{self.user.username} Profile'
+        return f'{self.user.username} Profile'  # pylint: disable=no-member
---
+++ users/signals.py
@@ -0,0 +1,15 @@
+""" Signals """
+# pylint: disable=unused-argument
+
+from django.dispatch import receiver
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+
+from users.models import Profile
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    """ Create a profile for new users """
+    if created:
+        Profile.objects.create(user=instance)
